638ce5437aeec75f07b73967cc638fb17cdd3b47
Tata2301010287 <tataabur@gmail.com>
2025-05-24 17:15:58 +0800

    Pertemuan10

---
+++ Latihan01/src/latihan01/Latihan01.java
@@ -0,0 +1,18 @@
+
+package latihan01;
+
+/**
+ *
+ * @author Antonia Tatarianingsih Abur
+ * Sabtu, 24 Mei 2025
+ */
+public class Latihan01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
